cb4e6f560f76d36e2dc86f8bdba426edde5f67ad
rananda1512 <rizkikeren1512@gmail.com>
2021-08-29 13:56:32 +0700

    firs commit

---
+++ android/app/src/main/kotlin/com/example/slicing_slide/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.slicing_slide
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor1 = Color(0xff142F43);
+Color backgroundColor2 = Color(0xffFFA929);
+Color backgroundColor3 = Color(0xff061A28);
+Color textColorBlack = Color(0xff082032);
+Color textColorRed = Color(0xffFF2929);
+Color textColorWhite = Colors.white;
+
+TextStyle textStyleBlack = GoogleFonts.poppins(
+  color: textColorBlack,
+);
+TextStyle textStyleWhite = GoogleFonts.poppins(
+  color: textColorWhite,
+);
+
+TextStyle textStyleYellow = GoogleFonts.poppins(
+  color: backgroundColor2,
+);
+
+TextStyle textStyleRed = GoogleFonts.poppins(
+  color: textColorRed,
+);
